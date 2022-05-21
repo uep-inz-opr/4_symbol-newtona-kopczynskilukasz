6ef7d1f5f9e92fca0fa46113aa4473a879f1ff6c
kopczynskilukasz <102819157+kopczynskilukasz@users.noreply.github.com>
2022-05-21 10:02:11 +0200

    ostateczna wersja

--- main.py
+++ main.py
@@ -6,18 +6,6 @@ numbers = [int(i) for i in x]
 n = numbers[0]
 k = numbers[1]
 
-
-
-# if y > x or len(numbers)>2 :
-#     print(0)
-# elif y == 1 or y == x: print(1)
-# else:
-#     a = math.factorial(x)
-#     b = math.factorial(y)
-#     div = a // (b*(x-y))
-#     print(div)
-
-
 def Newton(n, k):
     Wynik = 1
     for i in range(1, k + 1):
